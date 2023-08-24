4c01c01b74e9bef7f9a705c7f4483d85d356fce5
caiovictormc <caiovictormc@gmail.com>
2023-08-24 15:07:24 -0300

    zsh aliases

---
+++ zsh/zshrc.sh
@@ -0,0 +1,11 @@
+function main() {
+    setup_aliases
+}
+
+function setup_aliases() {
+    alias pmr="python manage.py runserver"
+    alias pms="python manage.py shell"
+    alias pmsp="python manage.py shell_plus"
+}
+
+main
