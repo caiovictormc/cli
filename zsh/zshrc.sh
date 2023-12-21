e4866d1355af9c0ecde14118d945872f3da2e3c3
caiovictormc <caiovictormc@gmail.com>
2023-12-21 11:45:54 -0300

    new alias dcd

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -6,6 +6,7 @@ function setup_aliases() {
     alias pmr="python manage.py runserver"
     alias pms="python manage.py shell"
     alias pmsp="python manage.py shell_plus"
+    alias dcd="docker-compose -f docker-compose.dev.yml"
 }
 
 main
